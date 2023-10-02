907aab4863dcd7213066e248e836d7757d078f59
ValentinGhibaudo <vatghibaudo@gmail.com>
2023-10-02 09:22:57 +0200

    modif config path post change cmo team

--- configuration.py
+++ configuration.py
@@ -4,20 +4,20 @@ import getpass
 from pathlib import Path
 
 if getpass.getuser() == 'samuel' and  sys.platform.startswith('linux'):
-    base_folder = '/home/samuel/mnt/CRNLDATA//crnldata/cmo/Etudiants/NBuonviso2022_Sleep_EEG_Respi_Valentin/'
+    base_folder = '/home/samuel/mnt/CRNLDATA//crnldata/cmo/Projets/Autre/NBuonviso2022_Sleep_EEG_Respi_Valentin/'
     
 elif getpass.getuser() == 'samuel.garcia' and  sys.platform.startswith('linux'):
-    base_folder = '/crnldata/cmo/Etudiants/NBuonviso2022_Sleep_EEG_Respi_Valentin/'
+    base_folder = '/crnldata/cmo/Projets/Autre//NBuonviso2022_Sleep_EEG_Respi_Valentin/'
 
 elif getpass.getuser() == 'valentin' and  sys.platform.startswith('linux'):
-    base_folder = '/home/valentin/smb4k/CRNLDATA/crnldata/cmo/Etudiants/NBuonviso2022_Sleep_EEG_Respi_Valentin/'
+    base_folder = '/home/valentin/smb4k/CRNLDATA/crnldata/cmo/Projets/Autre//NBuonviso2022_Sleep_EEG_Respi_Valentin/'
     
 elif getpass.getuser() == 'valentin.ghibaudo' and  sys.platform.startswith('linux'):
-    base_folder = '/crnldata/cmo/Etudiants/NBuonviso2022_Sleep_EEG_Respi_Valentin/'
+    base_folder = '/crnldata/cmo/Projets/Autre//NBuonviso2022_Sleep_EEG_Respi_Valentin/'
 
 
 elif sys.platform.startswith('win'):
-	base_folder = 'N:/cmo/Etudiants/NBuonviso2022_Sleep_EEG_Respi_Valentin/'
+	base_folder = 'N:/cmo/Projets/Autre//NBuonviso2022_Sleep_EEG_Respi_Valentin/'
 
 
 base_folder = Path(base_folder)
